7afd2a52734de9803be14943d25871b318bbe0a4
Piyush Negi <piyushnegi381@gmail.com>
2026-07-10 23:01:08 +0530

    Time: 59 ms (23.44%), Space: 55.8 MB (55.87%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {
+    let reverse = 0;
+    let lastDigit;
+    while(x!=0){
+        lastDigit = Math.trunc(x%10);
+        x = Math.trunc(x/10);
+        reverse = reverse * 10 + lastDigit;
+    }
+    if (reverse < -(2 ** 31) || reverse > 2 ** 31 - 1) {
+    return 0;
+}
+    return reverse;
+};
