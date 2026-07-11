418a030ecac21f369a7bb94338fbed95124827ec
Piyush Negi <piyushnegi381@gmail.com>
2026-07-11 10:34:03 +0530

    Time: 2 ms (69.13%), Space: 59 MB (67.08%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+    let count = 0;
+  let maxCount = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) count++;
+    else count = 0;
+    if (count > maxCount) maxCount = count;
+  }
+  return maxCount;
+};
