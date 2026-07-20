9bc6618f1dc73f18746e0875f705a55d2c990940
Piyush Negi <piyushnegi381@gmail.com>
2026-07-20 21:18:57 +0530

    Time: 6 ms (28.13%), Space: 60.9 MB (43.75%) - LeetHub

---
+++ 1260-shift-2d-grid/1260-shift-2d-grid.js
@@ -0,0 +1,43 @@
+/**
+ * @param {number[][]} grid
+ * @param {number} k
+ * @return {number[][]}
+ */
+var reverse = function (arr, left, right) {
+  while (left < right) {
+    let temp = arr[left];
+    arr[left] = arr[right];
+    arr[right] = temp;
+    left++;
+    right--;
+  }
+};
+
+var rotate = function (arr, k) {
+  let n = arr.length;
+  k = k % n;
+  reverse(arr, 0, n - k - 1);
+  reverse(arr, n - k, n - 1);
+  reverse(arr, 0, n - 1);
+  return arr;
+};
+
+var shiftGrid = function(grid, k) {
+  let store = [];
+  let index = 0;
+  for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+        store.push(grid[i][j]);
+    }
+  }  
+
+  store = rotate(store,k);
+
+  for(let i = 0; i < grid.length; i++){
+    for(let j = 0; j < grid[i].length; j++){
+        grid[i][j] = store[index]
+        index++;
+    }
+  }  
+  return grid;
+};
