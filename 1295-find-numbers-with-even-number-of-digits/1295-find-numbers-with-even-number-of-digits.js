1b149464adedafee6da09cc927ace9aa7501e2b8
Piyush Negi <piyushnegi381@gmail.com>
2026-07-23 00:27:21 +0530

    Time: 1 ms (83.16%), Space: 54.4 MB (86.56%) - LeetHub

--- 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
+++ 1295-find-numbers-with-even-number-of-digits/1295-find-numbers-with-even-number-of-digits.js
@@ -3,12 +3,17 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
- let ans = 0;
-  for (let i = 0; i < nums.length; i++) {
-    let n = nums[i];
-    if ((n >= 10 && n <= 99) || (n >= 1000 && n <= 9999) || n === 100000) {
-      ans++;
+    let ans = 0;
+    for (let i = 0; i < nums.length; i++) {
+      let digits = 0;
+      let n = nums[i];
+      while (n > 0) {
+        digits++;
+        n = Math.trunc(n / 10);
+      }
+      if (digits % 2 === 0) {
+        ans++;
+      }
     }
-  }
-  return ans;
+    return ans;
 };
