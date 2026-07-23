38351a2f77572b991f96e9423864552905d77da3
Piyush Negi <piyushnegi381@gmail.com>
2026-07-24 00:10:58 +0530

    Time: 2 ms (73.66%), Space: 55.8 MB (89.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.js
+++ 0169-majority-element/0169-majority-element.js
@@ -3,9 +3,27 @@
  * @return {number}
  */
 var majorityElement = function (nums) {
-  let map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    map.set(nums[i], (map.get(nums[i]) || 0) + 1);
-    if(map.get(nums[i]) > nums.length / 2) return nums[i]
+  let elem;
+  let count = 0;
+
+  for(let i = 0; i < nums.length; i++) {
+    if(count === 0) {
+        elem = nums[i];
+        count++;
+    } else if (elem === nums[i]) {
+        count++;
+    } else {
+        count--;
+    }
   }
+
+  count = 0;
+  for(let i = 0; i < nums.length; i++) {
+    if(nums[i] === elem) {
+        count++
+    }
+  }
+  
+  if(count > nums.length / 2) return elem;
+  return -1;
 };
