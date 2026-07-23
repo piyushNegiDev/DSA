78d0a5aab3c5c63ca6f84cc48deb0202af2c258d
Piyush Negi <piyushnegi381@gmail.com>
2026-07-24 01:35:33 +0530

    Time: 2 ms (72.03%), Space: 63.4 MB (83.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,11 +3,13 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    let max = -Infinity;
+    let max = nums[0];
     let sum = 0;
     for(let i = 0; i < nums.length; i++) {
         sum+=nums[i];
-        max = Math.max(sum,max);
+        if(sum > max) {
+            max = sum;
+        }
         if(sum < 0 ) {
             sum = 0;
         }
