90421f4019fe00e025098dce2a82abc876796f75
Piyush Negi <piyushnegi381@gmail.com>
2026-07-30 17:05:03 +0530

    Time: 40 ms (33.51%), Space: 72.5 MB (55.25%) - LeetHub

---
+++ 0015-3sum/0015-3sum.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+
+var threeSum = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let n = nums.length;
+  let ans = [];
+
+  for (let i = 0; i < n; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let j = i + 1;
+    let k = n - 1;
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum < 0) {
+        j++;
+      } else if (sum > 0) {
+        k--;
+      } else {
+        ans.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (j < k && nums[j] === nums[j - 1]) j++;
+        while (j < k && nums[k] === nums[k + 1]) k--;
+      }
+    }
+  }
+
+  return ans;
+};
