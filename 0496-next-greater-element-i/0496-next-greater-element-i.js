29240ea53b9b0f6be8a072a75f59cf0b8a4e5302
Piyush Negi <piyushnegi381@gmail.com>
2026-08-19 19:28:02 +0530

    Time: 3 ms (68.08%), Space: 55.8 MB (90.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -4,20 +4,26 @@
  * @return {number[]}
  */
 var nextGreaterElement = function(nums1, nums2) {
-    var findNextGreater = function(value) {
-        let greater = -1;
-        for(let i = 0; i < nums2.length; i++) {
-            if(value === nums2[i]) {
-                for(let j = i + 1; j < nums2.length; j++) {
-                    if(nums2[j] > value) return nums2[j];
-                }
+    let map = new Map();
+
+    for(let i = 0; i < nums2.length; i++) {
+        if(i === nums2.length - 1) {
+            map.set(nums2[i], -1);
+            break;
+        }
+
+        for(let j = i + 1; j < nums2.length; j++) {
+            if(nums2[i] < nums2[j]) {
+                map.set(nums2[i], nums2[j]);
+                break;
+            } else if (j === nums2.length - 1) {
+                map.set(nums2[i], -1);
             }
         }
-        return greater;
     }
 
     for(let i = 0; i < nums1.length; i++) {
-        nums1[i] = findNextGreater(nums1[i]); 
+        nums1[i] = map.get(nums1[i]); 
     }
 
     return nums1;
