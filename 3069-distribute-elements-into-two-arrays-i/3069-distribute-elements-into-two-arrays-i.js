36d3b90191cc60ecb45d792e2f8c0dd1eccc565d
Piyush Negi <piyushnegi381@gmail.com>
2026-08-20 10:25:25 +0530

    Time: 2 ms (50%), Space: 56.6 MB (90.63%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.js
@@ -3,15 +3,11 @@
  * @return {number[]}
  */
 var resultArray = function(nums) {
-    let arr1 = [];
-    let arr2 = [];
+    let arr1 = [nums[0]];
+    let arr2 = [nums[1]];
 
-    for(let i = 0; i < nums.length; i++) {
-        if(arr1.length === 0) {
-            arr1.push(nums[i]);
-        } else if (arr2.length === 0) {
-            arr2.push(nums[i]);
-        } else if (arr1.at(-1) > arr2.at(-1)) {
+    for(let i = 2; i < nums.length; i++) {
+        if(arr1.at(-1) > arr2.at(-1)) {
             arr1.push(nums[i]);
         } else {
             arr2.push(nums[i]);
