87021b70fbd150abacb11171a77505fc0ef83044
Piyush Negi <piyushnegi381@gmail.com>
2026-08-22 13:04:51 +0530

    Time: 1 ms (66.2%), Space: 54.7 MB (84.48%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.js
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -3,22 +3,18 @@
  * @return {number}
  */
 var trap = function (h) {
-  let left = 0,
-    right = h.length - 1;
-  let leftMax = 0,
-    rightMax = 0;
+  let left = 0, right = h.length - 1;
+  let leftMax = 0, rightMax = 0;
   let water = 0;
 
   while (left < right) {
     if (h[left] <= h[right]) {
-      let check = leftMax - h[left];
-      if (check > 0) water += check;
       leftMax = Math.max(h[left], leftMax);
+      water += leftMax - h[left];
       left++;
     } else {
-      let check = rightMax - h[right];
-      if (check > 0) water += check;
       rightMax = Math.max(h[right], rightMax);
+      water += rightMax - h[right];
       right--;
     }
   }
