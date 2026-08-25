2b1e121d4e31c530edad49e29566560832d01584
Piyush Negi <piyushnegi381@gmail.com>
2026-08-25 23:39:29 +0530

    Time: 0 ms (100%), Space: 54.1 MB (48.91%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.js
@@ -3,30 +3,20 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    let ans = Infinity;
     let low = 0;
     let high = nums.length - 1;
 
     while(low <= high) {
         let mid = Math.floor((low + high) / 2);
 
-        if(nums[low] === nums[mid] && nums[mid] === nums[high]) {
-            ans = Math.min(ans, nums[low]);
-            low++;
-            high--;
-            continue;
-        }
-
         if(nums[mid] > nums[high]){
-            ans = Math.min(ans, nums[high])
             low = mid + 1;
         } else if (nums[mid] < nums[high]) {
-            ans = Math.min(ans, nums[mid]);
             high = mid;
         } else {
             high--;
         }
     }
     
-    return ans;
+    return nums[low];
 };
