e9c0db6edbca6f8acef620e292935cfad2762b83
Piyush Negi <piyushnegi381@gmail.com>
2026-08-26 20:29:41 +0530

    Time: 1 ms (100%), Space: 56.6 MB (60%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.js
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.js
@@ -4,34 +4,25 @@
  * @return {string}
  */
 var shortestBeautifulSubstring = function(s, k) {
-    let ans = '';
-    let len = Infinity;
     let left = 0;
-    let map = new Map();
-    let sMap = new Map();
+    let count = 0;
+    let ans = '';
 
-    for(let i = 0; i < s.length; i++) {
-        map.set(s[i], (map.get(s[i]) || 0) + 1);
-        ans += s[i];
-        while((map.get(s[i]) > k && s[i] === '1') || ans.startsWith('0')) {
-            ans = ans.slice(1);
-            map.set(s[left], (map.get(s[left])) - 1);
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '1') count++;
+
+        while (count === k) {
+            let candidate = s.slice(left, i + 1);
+
+            if (ans === '' || candidate.length < ans.length ||
+                (candidate.length === ans.length && candidate < ans)) {
+                ans = candidate;
+            }
+
+            if (s[left] === '1') count--;
             left++;
-        }
-        if(map.get('1') === k) {
-            sMap.set(ans, ans.length);
-            len = ans.length;
-        }
-    }
-
-    if(sMap.size === 0) return '';
-
-    for(let [key, value] of sMap) {
-        if(value <= len && Number(key) < Number(ans)) {
-            ans = key;
-            len = value;
         }
     }
 
     return ans;
-}; 
+};
