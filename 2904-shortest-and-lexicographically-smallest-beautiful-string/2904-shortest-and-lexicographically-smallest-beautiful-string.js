b106cc112b12f0b70273679275810154ab426765
Piyush Negi <piyushnegi381@gmail.com>
2026-08-26 11:26:09 +0530

    Time: 4 ms (46.67%), Space: 57.8 MB (33.33%) - LeetHub

---
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.js
@@ -0,0 +1,43 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var shortestBeautifulSubstring = function(s, k) {
+    let ans = '';
+    let len = Infinity;
+    let left = 0;
+    let map = new Map();
+    let sMap = new Map();
+
+    for(let i = 0; i < s.length; i++) {
+        map.set(s[i], (map.get(s[i]) || 0) + 1);
+        ans += s[i];
+        while((map.get(s[i]) > k && s[i] === '1') || ans.startsWith('0')) {
+            ans = ans.slice(1);
+            map.set(s[left], (map.get(s[left])) - 1);
+            left++;
+        }
+        if(map.get('1') === k) {
+            sMap.set(ans, ans.length);
+            len = ans.length;
+        }
+    }
+
+    // console.log(sMap);
+
+    if(sMap.size === 0) return '';
+
+    for(let [key, value] of sMap) {
+        if(value <= len && Number(key) < Number(ans)) {
+            ans = key;
+            len = value;
+        }
+    }
+
+    while(ans.endsWith('0')) {
+        ans = ans.slice(0, -1);
+    }
+
+    return ans;
+}; 
