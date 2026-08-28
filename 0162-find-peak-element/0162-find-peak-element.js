624c95caef9ffd83d2a61e779f7ab4ebba82b2a4
Piyush Negi <piyushnegi381@gmail.com>
2026-08-28 20:57:24 +0530

    Time: 0 ms (100%), Space: 53.8 MB (53.28%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.js
+++ 0162-find-peak-element/0162-find-peak-element.js
@@ -9,19 +9,17 @@ var findPeakElement = function(nums) {
     if(nums[n - 1] > nums[n - 2]) return n - 1;
 
     let low = 1;
-    let high = n - 1;
+    let high = n - 2;
 
     while(low <= high) {
         let mid = Math.floor((low + high) / 2);
-        
-        if(nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) {
-            return mid;
-        }
 
-        if(nums[mid] > nums[mid - 1] && nums[mid] < nums[mid] + 1) {
+        if(nums[mid] > nums[mid - 1] && nums[mid] < nums[mid + 1]) {
             low = mid + 1;
         } else {
             high = mid - 1;
         }
     }
+
+    return low;
 };
