d16078baf95142bc2247d032376d3d5d6dfc520f
Piyush Negi <piyushnegi381@gmail.com>
2026-08-29 14:03:43 +0530

    Time: 426 ms (20%), Space: 120.8 MB (0%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.js
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.js
@@ -3,35 +3,43 @@
  * @param {number} limit
  * @return {number[]}
  */
-var lexicographicallySmallestArray = function (nums, limit) {
+var lexicographicallySmallestArray = function(nums, limit) {
   let n = nums.length;
   let pairs = [];
 
-  for (let i = 0; i < n; i++) {
+  for(let i = 0; i < n; i++) {
     pairs.push([nums[i], i]);
-  }
+  }  
 
   pairs.sort((a, b) => a[0] - b[0]);
 
-  let ans = new Array(n);
-  let groupStart = 0;
+  let start = 0;
+  let groups = [];
+  let indices = [];
 
-  for (let i = 1; i <= n; i++) {
-    if (i === n || pairs[i][0] - pairs[i - 1][0] > limit) {
-      let groupIndices = [];
-      
-      for (let k = groupStart; k < i; k++) {
-        groupIndices.push(pairs[k][1]);
-      }
-      groupIndices.sort((a, b) => a - b);
-
-      for (let k = 0; k < groupIndices.length; k++) {
-        ans[groupIndices[k]] = pairs[groupStart + k][0];
-      }
-
-      groupStart = i;
+  for(let i = 1; i <= n; i++) {
+    if(i === n || pairs[i][0] - pairs[i - 1][0] > limit) {
+        let group = [];
+        let indice = [];
+        for(let j = start; j < i; j++) {
+            group.push(pairs[j][0]);
+            indice.push(pairs[j][1]);
+        }
+        groups.push(group);
+        indices.push(indice);
+        start = i;
     }
   }
 
-  return ans;
+  for(let i = 0; i < indices.length; i++) {
+    indices[i].sort((a, b) => a - b);
+  }
+
+  for(let i = 0; i < groups.length; i++) {
+    for(let j = 0; j < groups[i].length; j++) {
+        nums[indices[i][j]] = groups[i][j]
+    }
+  }
+
+  return nums;
 };
