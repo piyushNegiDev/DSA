71d1fbc828535db6cef1d3e68ab67b59b90bfdfa
Piyush Negi <piyushnegi381@gmail.com>
2026-08-30 11:23:40 +0530

    Time: 2 ms (86.21%), Space: 66.9 MB (89.66%) - LeetHub

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.js
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.js
@@ -4,6 +4,7 @@
  */
 var minimumDeletions = function(nums) {
     let n = nums.length;
+
     if(n === 1) return 1;
 
     let min = 0;
@@ -16,7 +17,7 @@ var minimumDeletions = function(nums) {
 
     let removeFromFront = max > min ? max + 1 : min + 1;
     let removeFromBack = max < min ?  n - max : n - min;
-    let removeFromBoth = min < max ? (min + 1) + (n - max) : (max + 1) + (n - min);
+    let removeFromBoth = min < max ? min + 1 + n - max : max + 1 + n - min;
 
     return Math.min(removeFromBoth, Math.min(removeFromFront, removeFromBack));
 };
