fb81747f5bddf9bc30bf3edb83ccec14c6580993
Piyush Negi <piyushnegi381@gmail.com>
2026-08-31 16:44:23 +0530

    Time: 0 ms (100%), Space: 52.8 MB (89.19%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    let low = 1;
+    let high = num;
+    let ans = false;
+
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if(mid * mid === num) {
+            return true;
+        } else if (mid * mid > num) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+};
