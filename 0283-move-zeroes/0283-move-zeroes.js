1e5db611574d5d721af87482ea2ae53c9a21db73
Piyush Negi <piyushnegi381@gmail.com>
2026-08-31 15:48:54 +0530

    Time: 5 ms (20.76%), Space: 61.3 MB (8.44%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,27 +2,12 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-    let slow = 0;
-    let fast = 0;
-    
-    while(slow < nums.length && fast < nums.length) {
-        if(nums[slow] === 0 && nums[fast] !== 0) {
-            let temp = nums[slow];
-            nums[slow] = nums[fast];
-            nums[fast] = temp;
-            slow++;
-            fast++;
-        }
-
-        while(nums[slow] !== 0 && slow < nums.length) {
-            slow++;
-        }
-
-        fast = slow;
-
-        while(nums[fast] === 0 && fast < nums.length) {
-            fast++;
-        }
+var moveZeroes = function (arr) {
+  let j = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== 0) {
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+      j++;
     }
+  }
 };
