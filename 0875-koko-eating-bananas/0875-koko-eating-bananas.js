6359ebf15c72aa2906c4bd562a58088dd7e1011e
Piyush Negi <piyushnegi381@gmail.com>
2026-09-01 12:03:16 +0530

    Time: 8 ms (75.34%), Space: 57 MB (87.52%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} piles
+ * @param {number} h
+ * @return {number}
+ */
+var minEatingSpeed = function(piles, h) {
+    let maxInArr = piles[0];
+    let n = piles.length;
+
+    for(let i = 1; i < n; i++) {
+        maxInArr = Math.max(piles[i], maxInArr);
+    }
+
+    let ans = maxInArr;
+    let low = 1;
+    let high = maxInArr;
+
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let hours = 0;
+
+        for(let i = 0; i < n; i++) {
+            hours += Math.ceil(piles[i] / mid);
+        }
+        
+        if(hours <= h) {
+            ans = Math.min(mid, ans);
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+};
