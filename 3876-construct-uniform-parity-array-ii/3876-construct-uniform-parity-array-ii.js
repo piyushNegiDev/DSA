0ae36bb616d2e6ee650478422394cfbf0ec2b36e
Piyush Negi <piyushnegi381@gmail.com>
2026-09-03 17:00:52 +0530

    Time: 20 ms (9.09%), Space: 83.6 MB (9.09%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.js
@@ -0,0 +1,42 @@
+/**
+ * @param {number[]} nums1
+ * @return {boolean}
+ */
+var uniformArray = function(nums1) {
+    let n = nums1.length;
+    if(n === 1) return true;
+    
+    let smallestEven = Infinity;
+    let smallestOdd = Infinity;
+
+    let evenArr = [];
+    let oddArr = [];
+
+    for(let i = 0; i < n; i++) {
+        if(nums1[i] % 2 === 0 && nums1[i] < smallestEven) {
+            smallestEven = nums1[i];
+        } else if(nums1[i] % 2 !== 0 && nums1[i] < smallestOdd) {
+            smallestOdd = nums1[i];
+        }
+    }
+
+    for(let i = 0; i < n; i++) {
+        if(nums1[i] % 2 === 0) {
+            evenArr.push(nums1[i]);
+        } else if(nums1[i] - smallestOdd >= 1) {
+            evenArr.push(nums1[i] - smallestOdd);
+        }
+    }
+    if(evenArr.length === n) return true;
+
+    for(let i = 0; i < n; i++) {
+        if(nums1[i] % 2 !== 0) {
+            oddArr.push(nums1[i]);
+        } else if(nums1[i] - smallestOdd >= 1) {
+            oddArr.push(nums1[i] - smallestOdd);
+        }
+    }
+    if(oddArr.length === n) return true;
+    
+    return false;
+};
