d3415841caffb326369b8df7b11fffec70514f63
Piyush Negi <piyushnegi381@gmail.com>
2026-09-07 13:04:40 +0530

    Time: 0 ms (100%), Space: 53.4 MB (81.35%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let n = arr.length;
+
+    let low = 0;
+    let high = n - 1;
+
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
+
+        let missing = arr[mid] - (mid + 1);
+
+        if(missing < k) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    return high + 1 + k;
+};
