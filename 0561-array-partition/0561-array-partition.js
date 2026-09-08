5c86d7ab1383c086ff152053077be30cc2d29957
Piyush Negi <piyushnegi381@gmail.com>
2026-09-08 23:38:58 +0530

    Time: 42 ms (30.28%), Space: 61.1 MB (15.77%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arrayPairSum = function(nums) {
+    nums = nums.sort((a, b) => a - b);  
+
+    let sum = 0;
+
+    for(let i = 0; i < nums.length - 1; i = i + 2) {
+        sum += Math.min(nums[i], nums[i + 1]);
+    }
+
+    return sum;
+};
