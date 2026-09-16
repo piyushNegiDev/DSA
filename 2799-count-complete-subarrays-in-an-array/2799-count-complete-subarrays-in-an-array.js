40963bc1e2afa8e0ec9204d465cc2e8200d5c306
Piyush Negi <piyushnegi381@gmail.com>
2026-09-17 02:34:36 +0530

    Time: 2 ms (100%), Space: 62 MB (21.43%) - LeetHub

---
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var countCompleteSubarrays = function (nums) {
+  let map = new Map();
+  let n = nums.length;
+  let count = 0;
+
+  for (let i = 0; i < n; i++) {
+    map.set(nums[i], 0);
+  }
+
+  let left = 0;
+  let right = left + map.size - 1;
+
+  let windowMap = new Map();
+  for (let i = left; i < right; i++) {
+    windowMap.set(nums[i], (windowMap.get(nums[i]) || 0) + 1);
+  }
+
+  while (left < n) {
+    while(right < n && windowMap.size < map.size) {
+      windowMap.set(nums[right], (windowMap.get(nums[right]) || 0) + 1);
+      right++;
+    }
+    if (windowMap.size === map.size) {
+      count += n - right + 1;      
+    } 
+    windowMap.set(nums[left], windowMap.get(nums[left]) - 1);
+      if(windowMap.get(nums[left]) === 0) {
+        windowMap.delete(nums[left]);
+      }
+      left++;
+  }
+
+  return count;
+};
