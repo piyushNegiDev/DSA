b352b25a402ddab41d7312bc93986ae7333cd557
Piyush Negi <piyushnegi381@gmail.com>
2026-09-18 23:34:38 +0530

    Time: 7 ms (88.13%), Space: 59.5 MB (96.91%) - LeetHub

--- 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -4,27 +4,11 @@
  * @return {number[][]}
  */
 var findDifference = function(nums1, nums2) {
-    let map1 = new Map();
-    let map2 = new Map();
+    nums1 = new Set(nums1);
+    nums2 = new Set(nums2);
 
-    let distOfNums1 = [];
-    let distOfNums2 = [];
+    let ans1 = [...nums1].filter(n => !nums2.has(n));
+    let ans2 = [...nums2].filter(n => !nums1.has(n));
 
-    for(let i = 0; i < nums1.length; i++) {
-        map1.set(nums1[i], 0);
-    }
-
-    for(let i = 0; i < nums2.length; i++) {
-        map2.set(nums2[i], 0);
-    }
-
-    for(let i = 0; i < nums1.length; i++) {
-        if(!map2.has(nums1[i]) && !distOfNums1.includes(nums1[i])) distOfNums1.push(nums1[i]);
-    }
-
-    for(let i = 0; i < nums2.length; i++) {
-        if(!map1.has(nums2[i]) && !distOfNums2.includes(nums2[i])) distOfNums2.push(nums2[i]);
-    }
-
-    return [distOfNums1, distOfNums2];
+    return [ans1, ans2];
 };
