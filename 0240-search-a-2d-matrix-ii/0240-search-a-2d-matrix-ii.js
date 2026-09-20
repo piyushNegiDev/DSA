8ee0c8977582e11b94123be23472289ecf0a2055
Piyush Negi <piyushnegi381@gmail.com>
2026-09-20 15:21:40 +0530

    Time: 253 ms (81.58%), Space: 58.4 MB (65%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let row = 0;
+    let col = matrix[0].length - 1;
+
+    while(col >= 0 && row < matrix.length) {
+        if(matrix[row][col] === target) return true;
+        
+        if(matrix[row][col] > target) col--;
+        else row++;
+    }
+
+    return false;
+};
