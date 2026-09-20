61aa19274d5a836d2057840a437f565102e1bbac
Piyush Negi <piyushnegi381@gmail.com>
2026-09-20 12:47:59 +0530

    Time: 0 ms (100%), Space: 54.5 MB (15.32%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -3,35 +3,23 @@
  * @param {number} target
  * @return {boolean}
  */
-var searchRow = function (arr, target) {
-  let low = 0;
-  let high = arr.length - 1;
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
 
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
+    let low = 0;
+    let high = m * n - 1;
 
-    if (arr[mid] === target) return true;
+    while(low <= high) {
+        let mid = Math.floor((low + high) / 2);
 
-    if (arr[mid] > target) high = mid - 1;
-    else low = mid + 1;
-  }
+        let row = Math.floor(mid / n);
+        let col = mid % n;
 
-  return false;
-};
+        if(matrix[row][col] === target) return true;
+        else if(matrix[row][col] < target) low = mid + 1;
+        else high = mid - 1;
+    }
 
-var searchMatrix = function (arr, target) {
-  let low = 0;
-  let high = arr.length - 1;
-
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    
-    if (arr[mid][0] <= target && target <= arr[mid][arr[mid].length - 1])
-      return searchRow(arr[mid], target);
-
-    if (target < arr[mid][0]) high = mid - 1;
-    else low = mid + 1;
-  }
-
-  return false;
+    return false;
 };
