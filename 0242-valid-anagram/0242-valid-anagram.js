a04f081b462dbb717e9b627fc274f91bb06613be
Piyush Negi <piyushnegi381@gmail.com>
2026-09-21 21:27:12 +0530

    Time: 11 ms (78.9%), Space: 60.3 MB (17.87%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -3,21 +3,22 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    if(s.length !== t.length) return false;
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
 
-    let map = new Map();
-    for(let i = 0; i < s.length; i++) {
-        map.set(s[i], (map.get(s[i]) || 0) + 1);
-    }
+  let map = new Map();
 
-    for(let i = 0; i < t.length; i++) {
-        if(map.has(t[i]) && map.get(t[i]) > 0) {
-            map.set(t[i], map.get(t[i]) - 1);
-        } else {
-            return false;
-        }
-    } 
+  for (let char of s) {
+    map.set(char, (map.get(char) || 0) + 1);
+  }
 
-    return true;
+  for (let char of t) {
+    if (!map.has(char)) return false;
+
+    map.set(char, map.get(char) - 1);
+
+    if (map.get(char) < 0) return false;
+  }
+
+  return true;
 };
